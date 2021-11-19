1da64ca80ca77005a1bc627697d671e566376247
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2021-11-19 12:16:01 +0100

    add api test

---
+++ pkg/test/api_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func AddGroupTest(t *testing.T) {
+	resp, err := http.Get("http://localhost:80/multicast/v1/groups")
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Println(resp.Body)
+	}
+	assert.Equal(t, 1, 1)
+}
