8020f8814087bb44d74e0e81f05d9f10750aeeb1
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2021-11-25 10:33:50 +0100

    bug scmulticast pt2

--- pkg/MulticastScalarClock/orderedQueue.go
+++ pkg/MulticastScalarClock/orderedQueue.go
@@ -13,17 +13,17 @@ func (a OrderedMessages) Less(i, j int) bool { return a[i].Timestamp < a[j].Time
 func (a OrderedMessages) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 //func che ordina i messaggi in coda
-func (node *NodeSC) OrderingMessage() OrderedMessages {
+func (node *NodeSC) OrderingMessage() {
 	sort.Sort(node.ProcessingMessages)
 	log.Println("ordering messages", node.ProcessingMessages[0].Timestamp)
-	return node.ProcessingMessages
+	return
 }
 
 func (node *NodeSC) AddToQueue(m *MessageTimestamp, wg *sync.Mutex) {
 
 	log.Println("messaggio aggiunto in coda", node.NodeId)
 	node.ProcessingMessages = append(node.ProcessingMessages, *m)
-	node.ProcessingMessages = node.OrderingMessage()
+	node.OrderingMessage()
 	wg.Unlock()
 }
 
